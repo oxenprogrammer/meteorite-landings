5ee3bc26ae0df56273343f60c5520a3ecb702572
oxenprogrammer <emmy1000okello@gmail.com>
2019-05-20 14:34:51 +0300

    UPDATE: fix app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,27 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { AppService } from './app.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule,
+  MatSortModule, MatTableModule, MatFormFieldModule } from '@angular/material';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatFormFieldModule,
+        MatProgressSpinnerModule,
+        MatSortModule,
+        MatInputModule
+      ],
       declarations: [
         AppComponent
       ],
+      providers: [AppService],
     }).compileComponents();
   }));
 
@@ -16,16 +31,8 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'meteorite-landings'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('meteorite-landings');
+  it('should have the app service', () => {
+    expect(AppService).toBeTruthy();
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to meteorite-landings!');
-  });
 });
